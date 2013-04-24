2c6bda6aab3550eb66a84b51ebe2fb0f4dfc9ea4
FND <fnd@localhost.localdomain>
2013-04-24 15:33:17 +0200

    added custom note class

---
+++ app/models/note/sample/zip_code.rb
@@ -0,0 +1,8 @@
+# encoding: UTF-8
+
+class Note::Sample::ZipCode < Note::SKOS::Base
+
+  self.rdf_namespace = 'sample'
+  self.rdf_predicate = 'zipcode'
+
+end
--- config/initializers/iqvoc.rb
+++ config/initializers/iqvoc.rb
@@ -4,10 +4,4 @@ Iqvoc.config do |config|
   })
 end
 
-# Iqvoc::Concept.base_class_name = "MyConceptClass"
-# Iqvoc::Concept.pref_labeling_class_name = "MyLabelingClass"
-# Iqvoc::Concept.further_relation_class_names << "MyConceptRelationClass"
-# Iqvoc::Concept.note_class_names = []
-# Iqvoc.default_rdf_namespace_helper_methods << :my_namespace
-
-# Iqvoc.core_assets += []
+Iqvoc::Concept.note_class_names = ["Note::Sample::ZipCode"]
