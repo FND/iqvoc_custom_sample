d77d344d6aeccb2cf51f08381ed3dabe15375c0f
FND <fnd@localhost.localdomain>
2013-04-24 15:51:47 +0200

    added separate partial for custom note
    adapted from iQvoc's `app/views/partials/note/_edit_base.html.erb`

--- app/models/note/sample/zip_code.rb
+++ app/models/note/sample/zip_code.rb
@@ -5,4 +5,8 @@ class Note::Sample::ZipCode < Note::SKOS::Base
   self.rdf_namespace = 'sample'
   self.rdf_predicate = 'zipcode'
 
+  def self.edit_partial_name(obj)
+    "partials/note/sample/edit_zip_code"
+  end
+
 end
